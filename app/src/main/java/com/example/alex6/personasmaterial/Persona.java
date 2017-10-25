4f87d6f1454b2066f3914e90a09f3151b2203a11
“Alexhamb” <alex6_10@hotmail.com>
2017-10-24 21:07:46 -0500

    clase terminada

--- app/src/main/java/com/example/alex6/personasmaterial/Persona.java
+++ app/src/main/java/com/example/alex6/personasmaterial/Persona.java
@@ -5,11 +5,16 @@ package com.example.alex6.personasmaterial;
  */
 
 public class Persona {
+    private String id;
     private int foto;
     private String cedula;
     private String nombre;
     private String apellido;
     private int sexo;
+
+    public Persona(){
+
+    }
 
     public Persona(int foto, String cedula, String nombre, String apellido, int sexo) {
         this.foto = foto;
@@ -24,6 +29,15 @@ public class Persona {
         this.cedula = cedula;
         this.nombre = nombre;
         this.apellido = apellido;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public int getFoto() {
